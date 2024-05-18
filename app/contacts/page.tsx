fce9d572918e4810513b315a8f447ec577f85568
fahminurcahya <fahminurcahya@gmail.com>
2024-05-18 22:24:19 +0700

    pagination

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,9 +1,11 @@
 import { CreateButton } from '@/components/button'
 import ContactTable from '@/components/contact-table'
+import Pagination from '@/components/pagination'
 import Search from '@/components/search'
+import { getContactPages } from '@/lib/data'
 import React from 'react'
 
-export default function Contact(
+export default async function Contact(
     { searchParams }:
         {
             searchParams?: {
@@ -14,6 +16,8 @@ export default function Contact(
 ) {
     const query = searchParams?.query || "";
     const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await getContactPages(query);
+
     return (
         <div className='max-w-screen-md mx-auto mt-5'>
             <div className='flex items-center justify-between gap-1 mb-5'>
@@ -21,6 +25,9 @@ export default function Contact(
                 <CreateButton />
             </div>
             <ContactTable query={query} currentPage={currentPage} />
+            <div className='flex justify-center mt-4'>
+                <Pagination totalPages={totalPages} />
+            </div>
         </div>
     )
 }
--- components/search.tsx
+++ components/search.tsx
@@ -12,8 +12,8 @@ export default function Search() {
     const { replace } = useRouter();
 
     const handleChange = useDebouncedCallback((term: string) => {
-        console.log(term);
         const params = new URLSearchParams(searchParams);
+        params.set("page", "1");
         if (term) {
             params.set("query", term);
         } else {
